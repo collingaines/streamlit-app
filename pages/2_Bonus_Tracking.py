8db98c43b12fba944c3933411ebedf24b0ee6a5f
collingaines <105612696+collingaines@users.noreply.github.com>
2025-01-27 14:46:32 -0600

    Update 2_Bonus_Tracking.py

--- pages/2_Bonus_Tracking.py
+++ pages/2_Bonus_Tracking.py
@@ -216,7 +216,7 @@ if userPosition!=None:
         #=====================================================================================================================================================================================
         #SECTION A | Title Section:
         #region CLICK HERE TO EXPAND SECTION 
-        st.title('Bonus F Tracking')
+        st.title('Bonus Tracking')
 
 
         st.markdown(
